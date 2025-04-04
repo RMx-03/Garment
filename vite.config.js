d02324f3146f371ebd9b32a7c940e7c7ce3b8aa2
RMx-03 <140522220+RMx-03@users.noreply.github.com>
2025-04-04 23:36:32 +0530

    handled image optimizer issues

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { ViteImageOptimizer } from 'vite-plugin-image-optimizer'
 
 export default defineConfig({
   plugins: [
-    react(),
-    ViteImageOptimizer({
-      jpg: {
-        quality: 80,
-      },
-      jpeg: {
-        quality: 80,
-      },
-      png: {
-        quality: 80,
-      },
-      webp: {
-        quality: 80,
-      }
-    })
+    react()
   ],
   build: {
     rollupOptions: {
